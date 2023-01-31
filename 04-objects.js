5b6ed237c02fe8b9cef9261af08f6f16a59fa5a5
Ganesh K <ganeshkannan627@gmail.com>
2023-01-31 22:50:09 +0530

    adding objects

---
+++ 04-objects.js
@@ -0,0 +1,20 @@
+const summa = {};
+console.log(summa)
+
+
+harry = {
+      "id": 7,
+      "name": "Harry Potter",
+      "patronus": "Stag",
+      "pet": "Hedwig the Snow Owl",
+      "+bonus_points": 1000
+    }
+
+console.log(harry)
+console.log(harry["name"])
+console.log(harry["+bonus_points"])
+
+harry.name="Harry James Potter"
+console.log(harry)
+
+
