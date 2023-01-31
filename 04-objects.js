2ccdda7539d268c9ddd4400a0460edb9a1de291c
Ganesh K <ganeshkannan627@gmail.com>
2023-01-31 23:18:31 +0530

    updating object with property access

--- 04-objects.js
+++ 04-objects.js
@@ -1,7 +1,7 @@
 const summa = {};
 console.log(summa)
 
-
+// creating object literals
 harry = {
       id: 7,
       name: "Harry Potter",
@@ -14,7 +14,8 @@ harry = {
     }
 
 console.log(harry)
-console.log(harry["name"])
+console.log(harry.name) // using .(dot) operator to access properties in an object
+console.log(harry["name"]) // using [](bracket) to access properties in an object
 console.log(harry["+bonus_points"])
 console.warn(harry.shout)
 console.warn(harry.shout())
