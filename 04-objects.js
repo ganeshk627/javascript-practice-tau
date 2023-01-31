b9a7f96dd90a29c846d7da9be07879a2f80d5b67
Ganesh K <ganeshkannan627@gmail.com>
2023-01-31 23:04:57 +0530

    updating objects

--- 04-objects.js
+++ 04-objects.js
@@ -3,16 +3,21 @@ console.log(summa)
 
 
 harry = {
-      "id": 7,
-      "name": "Harry Potter",
-      "patronus": "Stag",
-      "pet": "Hedwig the Snow Owl",
-      "+bonus_points": 1000
+      id: 7,
+      name: "Harry Potter",
+      patronus: "Stag",
+      pet: "Hedwig the Snow Owl",
+      "+bonus_points": 1000,
+      shout: function(){
+        console.log("***SHOUTED***")
+      }
     }
 
 console.log(harry)
 console.log(harry["name"])
 console.log(harry["+bonus_points"])
+console.warn(harry.shout)
+console.warn(harry.shout())
 
 harry.name="Harry James Potter"
 console.log(harry)
